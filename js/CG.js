406f437e1a1ce169c5381ca22c51189ff6e8a9a5
barwan12 <amitkumar.aec05@gmail.com>
2021-08-19 15:53:07 +0530

    some unused code is removed.

--- js/CG.js
+++ js/CG.js
@@ -1,17 +1,10 @@
 function TabContentOver(TabName){
-   let btn =  document.getElementsByClassName("btnclass");
-   // alert("Over")
-   // console.log(btn);
-   console.log(TabName);
-   let content = document.getElementsByClassName("tabContent");
-   let counts = content.length;
-   console.log(counts);
+   let Overbtn =  document.getElementsByClassName("btnclass");
     let para = document.getElementById(TabName);
     para.style.display="block";
 }
 function TabContentOut(TabName){
-   let btn =  document.getElementsByClassName("btnclass");
-   // alert("Out");
+   let Outbtn =  document.getElementsByClassName("btnclass");
    let para = document.getElementById(TabName);
    para.style.display="none";
 }
