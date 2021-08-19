a3b180b928062e1b4748bc138c46487f35f250d7
barwan12 <amitkumar.aec05@gmail.com>
2021-08-19 16:49:05 +0530

    Ist tab event corrected.

--- js/CG.js
+++ js/CG.js
@@ -1,10 +1,17 @@
-function TabContentOver(TabName){
+function TabContentOver(event,TabName){
    let Overbtn =  document.getElementsByClassName("btnclass");
+   let tabcontents = document.getElementsByClassName('tabContent');
+   for(let i=0;i<tabcontents.length;++i)
+   {
+      tabcontents[i].style.display="none";
+   }
+
     let para = document.getElementById(TabName);
     para.style.display="block";
+   //  evt.currentTarget.className += " active";
 }
-function TabContentOut(TabName){
-   let Outbtn =  document.getElementsByClassName("btnclass");
-   let para = document.getElementById(TabName);
-   para.style.display="none";
-}
+// function TabContentOut(TabName){
+//    let Outbtn =  document.getElementsByClassName("btnclass");
+//    let para = document.getElementById(TabName);
+//    para.style.display="none";
+// }
