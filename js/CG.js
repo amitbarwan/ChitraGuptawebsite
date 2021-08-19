b89557f609907bd99ad84806499583585203739d
barwan12 <amitkumar.aec05@gmail.com>
2021-08-19 15:50:30 +0530

    1st tab content is added

--- js/CG.js
+++ js/CG.js
@@ -1,24 +1,17 @@
-function TabContent(){
-   let btn =  document.getElementById("btnclass");
-   console.log(btn);
+function TabContentOver(TabName){
+   let btn =  document.getElementsByClassName("btnclass");
+   // alert("Over")
+   // console.log(btn);
+   console.log(TabName);
    let content = document.getElementsByClassName("tabContent");
    let counts = content.length;
-   for(let i=0;i<counts;++i)
-   {
-        content[i].style.display = "none";
-
-   }
-//    console.log(counts);
-//     let tabcontent =  document.getElementById("btnHome1");
-//    if(btn!="none")
-//    {
-//     alert("If"); 
-//     tabcontent.style.display="Active";
-//    }
-//    else
-//    {
-//        alert("Else");
-//     tabcontent.style.display="none";
-//    }
-  
+   console.log(counts);
+    let para = document.getElementById(TabName);
+    para.style.display="block";
+}
+function TabContentOut(TabName){
+   let btn =  document.getElementsByClassName("btnclass");
+   // alert("Out");
+   let para = document.getElementById(TabName);
+   para.style.display="none";
 }
